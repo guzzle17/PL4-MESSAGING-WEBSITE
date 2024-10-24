d1df599859726f923f2dd03d87c139a780758e09
thuonguyenvan <thuongnguyenvan2209@gmail.com>
2024-10-24 14:35:55 +0700

    upd folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { React, useState } from "react";
-import SearchBar from './components/SearchBar';
+import SearchBar from './Components/SearchBar';
 
 function App() {
   return (
