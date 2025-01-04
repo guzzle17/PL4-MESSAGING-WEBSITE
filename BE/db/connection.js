cb0d36332c894bdd2cf0a2239eafcf81aa47f1f1
thuonguyenvan <thuongnguyenvan2209@gmail.com>
2025-01-04 19:13:33 +0700

    update folder structure

--- BE/db/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const url = 'mongodb+srv://pbl4admin:pbl4admin@cluster0.x14gr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 '
-mongoose.connect(url, {
-    dbName: 'test'
-})
-    .then(() => console.log("Connect successful"))
-    .catch((e) => console.log("Error:" + e));
